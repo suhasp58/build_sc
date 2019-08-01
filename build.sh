428b2075437d3225f915b9f7eda36b038400095b
suhasp58 <52521805+suhasp58@users.noreply.github.com>
2019-08-01 12:46:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ mvn clean install
 cd ..
 
 docker build -t data-pusher-service1:v1 ./input-data
-sed -e 's/jsonfile1.txt/jsonfile2.txt/g' -e 's/-Dserver.port=8081/-Dserver.port=8082/g' -e 's/8081/8082/g' ./input-data/Dockerfile
+sed -i -e 's/jsonfile1.txt/jsonfile2.txt/g' -e 's/-Dserver.port=8081/-Dserver.port=8082/g' -e 's/8081/8082/g' ./input-data/Dockerfile
 docker build -t data-pusher-service2:v1 ./input-data
-sed -e 's/jsonfile2.txt/jsonfile3.txt/g' -e 's/-Dserver.port=8082/-Dserver.port=8083/g' -e 's/8082/8083/g' ./input-data/Dockerfile
+sed -i -e 's/jsonfile2.txt/jsonfile3.txt/g' -e 's/-Dserver.port=8082/-Dserver.port=8083/g' -e 's/8082/8083/g' ./input-data/Dockerfile
 docker build -t data-pusher-service3:v1 ./input-data
-sed  -e 's/jsonfile3.txt/jsonfile4.txt/g' -e 's/-Dserver.port=8083/-Dserver.port=8084/g' -e 's/8083/8084/g'  ./input-data/Dockerfile
+sed  -i -e 's/jsonfile3.txt/jsonfile4.txt/g' -e 's/-Dserver.port=8083/-Dserver.port=8084/g' -e 's/8083/8084/g'  ./input-data/Dockerfile
 docker build -t data-pusher-service4:v1 ./input-data
 docker build -t redisdb:v1 ./input-data/redis/
 cd input-data 
