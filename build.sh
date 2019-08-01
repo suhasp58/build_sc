9c94d2472cf6e3665ae65169e2dfce2e5b24f93d
suhasp58 <52521805+suhasp58@users.noreply.github.com>
2019-08-01 11:40:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ git clone https://github.com/suhasp58/input-data.git
 cd data-pusher
 mvn clean install
 
-docker build -t data-pusher-service1:v1 ./data-pusher
-sed -e 's/jsonfile1.txt/jsonfile2.txt/g' -e 's/-Dserver.port=8081/-Dserver.port=8082/g' -e 's/8081/8082/g' -e 's/./input-file/jsonfile1/./input-file/jsonfile2/g' ./data-pusher/Dockerfile
-docker build -t data-pusher-service2:v1 ./data-pusher
-sed -e 's/jsonfile2.txt/jsonfile3.txt/g' -e 's/-Dserver.port=8082/-Dserver.port=8083/g' -e 's/8082/8083/g' -e 's/./input-file/jsonfile2/./input-file/jsonfile3/g' ./data-pusher/Dockerfile
-docker build -t data-pusher-service3:v1 ./data-pusher
-sed  -e 's/jsonfile3.txt/jsonfile4.txt/g' -e 's/-Dserver.port=8083/-Dserver.port=8084/g' -e 's/8083/8084/g' -e 's/./input-file/jsonfile3/./input-file/jsonfile4/g' ./data-pusher/Dockerfile
-docker build -t data-pusher-service4:v1 ./data-pusher
+docker build -t data-pusher-service1:v1 ./input-data
+sed -e 's/jsonfile1.txt/jsonfile2.txt/g' -e 's/-Dserver.port=8081/-Dserver.port=8082/g' -e 's/8081/8082/g' -e 's/.\/input-file/jsonfile1/.\/input-file/jsonfile2/g' ./input-data/Dockerfile
+docker build -t data-pusher-service2:v1 ./input-data
+sed -e 's/jsonfile2.txt/jsonfile3.txt/g' -e 's/-Dserver.port=8082/-Dserver.port=8083/g' -e 's/8082/8083/g' -e 's/.\/input-file/jsonfile2/.\/input-file/jsonfile3/g' ./input-data/Dockerfile
+docker build -t data-pusher-service3:v1 ./input-data
+sed  -e 's/jsonfile3.txt/jsonfile4.txt/g' -e 's/-Dserver.port=8083/-Dserver.port=8084/g' -e 's/8083/8084/g' -e 's/.\/input-file/jsonfile3/.\/input-file/jsonfile4/g' ./input-data/Dockerfile
+docker build -t data-pusher-service4:v1 ./input-data
 
 cd input-data 
 docker-compose up 
