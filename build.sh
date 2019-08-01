377b497a6282bb6d357531793f99702e8e98d101
suhasp58 <52521805+suhasp58@users.noreply.github.com>
2019-08-01 11:26:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ git clone https://github.com/suhasp58/File.git
 cd build_sc/data-pusher
 mvn clean install
 
-cp docker-compose.yml build_sc/docker-compose.yml
+docker build -t data-pusher-service1:v1 ./data-pusher
+sed -e 's/jsonfile1.txt/jsonfile2.txt/g' -e 's/-Dserver.port=8081/-Dserver.port=8082/g' -e 's/8081/8082/g' -e 's/./input-file/jsonfile1/./input-file/jsonfile2/g' ./data-pusher/Dockerfile
+docker build -t data-pusher-service2:v1 ./data-pusher
+sed -e 's/jsonfile2.txt/jsonfile3.txt/g' -e 's/-Dserver.port=8082/-Dserver.port=8083/g' -e 's/8082/8083/g' -e 's/./input-file/jsonfile2/./input-file/jsonfile3/g' ./data-pusher/Dockerfile
+docker build -t data-pusher-service3:v1 ./data-pusher
+sed  -e 's/jsonfile3.txt/jsonfile4.txt/g' -e 's/-Dserver.port=8083/-Dserver.port=8084/g' -e 's/8083/8084/g' -e 's/./input-file/jsonfile3/./input-file/jsonfile4/g' ./data-pusher/Dockerfile
+docker build -t data-pusher-service4:v1 ./data-pusher
 
-cd build_sc
+
 docker-compose up --build
